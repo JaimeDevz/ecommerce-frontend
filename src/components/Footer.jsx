99335fe05baace7a87fdcf651a63623047eaabf7
JaimeDevz <jaimetorres-1@outlook.com>
2025-06-03 09:44:34 -0700

    prepare for deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,6 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useParams, useNavigate } from "react-router-dom";
-import products from "../data/products";
-import { useDispatch } from "react-redux";
-import { addToCart } from "../features/cart/cartSlice";
-import { useSelector } from "react-redux";
 
 export default function Footer() {
   const footerRef = useRef(null);
@@ -26,29 +21,7 @@ export default function Footer() {
     >
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
         <h2 className="text-2xl font-semibold">Solstice Brews</h2>
-        <div className="flex gap-6 text-xl">
-          <a
-            href="#"
-            className="hover:text-blue-500  transition-all duration-300"
-            aria-label="Twitter"
-          >
-            t
-          </a>
-          <a
-            href="#"
-            className="hover:text-blue-500  transition-all duration-300"
-            aria-label="Instagram"
-          >
-            i
-          </a>
-          <a
-            href="#"
-            className="hover:text-blue-500  transition-all duration-300"
-            aria-label="YouTube"
-          >
-            yo
-          </a>
-        </div>
+
         <p className="text-sm text-gray-400">
           © {new Date().getFullYear()} Solstice Brews. All rights reserved.
         </p>
